94d69da67a867e60b80e0466a22bf5d1ef1e71c0
Leonel Dorneles Porto <leonel.porto@compasso.com.br>
2021-07-20 02:19:06 -0300

    Teste

--- src/main/java/br/com/easylearn/controller/AlunoController.java
+++ src/main/java/br/com/easylearn/controller/AlunoController.java
@@ -19,7 +19,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("v1/a")
+@RequestMapping("v1/aluno")
 //@PreAuthorize("hasRole('ALUNO')")
 public class AlunoController {
 
