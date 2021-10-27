008187507a43f1048db75c893798a729868f144b
Leonel Dorneles Porto <leonel.porto@compasso.com.br>
2021-10-27 11:10:24 -0300

    Ajustando a entidade aula, para aceitar curso e modulo no crud.

--- src/main/java/br/com/easylearn/domain/Modulo.java
+++ src/main/java/br/com/easylearn/domain/Modulo.java
@@ -15,13 +15,6 @@ public class Modulo extends AbstractEntity{
     private Integer indice;
     private String titulo;
     private String tituloSecundario;
-    @ElementCollection
-    private List<String> subtitulo = new ArrayList<>();
-    @JsonIgnore
-    @OneToOne
-    private Curso curso;
-    @OneToMany
-    private List<Aula> aulaList;
 
     public Modulo(Integer indice, String titulo, String tituloSecundario) {
         this.indice = indice;
@@ -55,29 +48,5 @@ public class Modulo extends AbstractEntity{
 
     public void setTituloSecundario(String tituloSecundario) {
         this.tituloSecundario = tituloSecundario;
-    }
-
-    public List<String> getSubtitulo() {
-        return subtitulo;
-    }
-
-    public void setSubtitulo(List<String> subtitulo) {
-        this.subtitulo = subtitulo;
-    }
-
-    public Curso getCurso() {
-        return curso;
-    }
-
-    public void setCurso(Curso curso) {
-        this.curso = curso;
-    }
-
-    public List<Aula> getAulaList() {
-        return aulaList;
-    }
-
-    public void setAulaList(List<Aula> aulaList) {
-        this.aulaList = aulaList;
     }
 }
