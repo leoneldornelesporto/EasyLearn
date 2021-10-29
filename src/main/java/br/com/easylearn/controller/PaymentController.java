842ddb31ea170ddce1e7f2aecb86511ddefc5546
Leonel Dorneles Porto <leonel.porto@compasso.com.br>
2021-10-29 09:45:45 -0300

    Ajustando a entidade aula, para aceitar curso e modulo no crud.

--- src/main/java/br/com/easylearn/controller/PaymentController.java
+++ src/main/java/br/com/easylearn/controller/PaymentController.java
@@ -35,4 +35,10 @@ public class PaymentController {
         else
             return ResponseEntity.ok(save);
     }
+
+    @DeleteMapping("/todos")
+    public Boolean b(){
+        paymentRepository.deleteAll();
+        return Boolean.TRUE;
+    }
 }
