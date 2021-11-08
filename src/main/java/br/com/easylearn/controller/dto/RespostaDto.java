05db56ba7a72ccb57b9d5769101ab2425242574b
Leonel Dorneles Porto <leonel.porto@compasso.com.br>
2021-11-07 22:38:34 -0300

    Ajustando a entidade aula, para aceitar curso e modulo no crud.

--- src/main/java/br/com/easylearn/controller/dto/RespostaDto.java
+++ src/main/java/br/com/easylearn/controller/dto/RespostaDto.java
@@ -7,13 +7,11 @@ import java.time.LocalDateTime;
 public class RespostaDto {
     private Long id;
     private String mensagem;
-    private LocalDateTime dataCriacao;
     private String nomeAutor;
 
     public RespostaDto(Resposta resposta) {
         this.id = resposta.getId();
         this.mensagem = resposta.getMensagem();
-        this.dataCriacao = resposta.getDataCriacao();
         this.nomeAutor = resposta.getAutor().getNomeCompleto();
     }
 
@@ -23,10 +21,6 @@ public class RespostaDto {
 
     public String getMensagem() {
         return mensagem;
-    }
-
-    public LocalDateTime getDataCriacao() {
-        return dataCriacao;
     }
 
     public String getNomeAutor() {
